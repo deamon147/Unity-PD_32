769105de5da80e9f003378686c0846b3da6a842f
deamon147 <55359388+deamon147@users.noreply.github.com>
2020-05-29 22:20:04 +0300

    git add
    git add

---
+++ Assets/Scripts/Balloon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Balloon : MonoBehaviour {
+
+	private Text ScoreText;
+
+	private void Start () {
+		ScoreText = GameManager.gm.GameplayUI.Find ("ScorePlaceholder").Find ("Score").GetComponent<Text> ();
+	}
+	
+	private void Update () {
+		ScoreText.text = Mathf.Max (0, Mathf.FloorToInt (transform.position.y)).ToString ();
+	}
+
+	private void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag == "Obstacle") {
+			print ("GAME OVER!");
+			SceneManager.LoadScene ("Menu");
+;		}
+		else if (other.tag == "LevelEnd") {
+			other.tag = "Untagged"; 
+			GameManager.gm.lm.SpawnLevel ();
+		}
+	}
+
+}
---
+++ Assets/Scripts/ButtonStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonStart : MonoBehaviour
+{
+
+    public string action;
+
+    void OnMouseUpAsButton()
+    {
+        switch (action)
+        {
+            case "start":
+                Application.LoadLevel("Gameplay");
+                break;
+        }
+    }
+}
